a57563ee205d06f1cc1a3a9099ad06a6331feafa
Sebastian Velasco <sesbas778@gmail.com>
2024-04-11 20:45:11 -0500

    se sube al repo

---
+++ src/router.tsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Products, {
+  loader as productLoader,
+  action as updateAvailabilityAction,
+} from "./views/Products";
+import NewProduct, { action as newProductAction } from "./views/NewProduct";
+import EditProduct, {
+  loader as editProductLoader,
+  action as editProductAction,
+} from "./views/EditProduct";
+import { action as deleteProductAction } from "./components/ProductDetails";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Products />,
+        loader: productLoader,
+        action: updateAvailabilityAction,
+      },
+      {
+        path: "/productos/nuevo",
+        element: <NewProduct />,
+        action: newProductAction,
+      },
+      {
+        path: "/productos/:id/editar", //ROA pattern - Resource-oriented design
+        element: <EditProduct />,
+        loader: editProductLoader,
+        action: editProductAction,
+      },
+      {
+        path: "/productos/:id/eliminar",
+        action: deleteProductAction,
+      },
+    ],
+  },
+]);
